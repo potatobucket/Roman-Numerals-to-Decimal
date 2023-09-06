bd99c9f2578a2110b5a4290a59ca333aec4452b9
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-09-06 09:18:21 -0500

    Just barely not working
    It's leaving off the last numeral. This is a thing that can be conquered, I'm just not sure how eloquently.

--- roman_numerals.py
+++ roman_numerals.py
@@ -10,27 +10,50 @@ numerals = {
 
 running = True
 testNumber = "MMM"
+# testNumber = "MCMLXXXVI"
+# testNumber = "IV"
+total = 0
 
-def check_if_input_is_in_range():
-    userInput = int(input("What roman number do you want to convert? (1 - 3,000)\n"))
-    if userInput < 1 or userInput > 3000:
-        userInput = int(input("Please enter a number between 1 and 3,000.\n"))
-        return "Number outside of operating parameters."
-    else:
-        return userInput
+# def check_if_input_is_in_range():
+#     userInput = int(input("What roman number do you want to convert? (1 - 3,000)\n"))
+#     if userInput < 1 or userInput > 3000:
+#         userInput = int(input("Please enter a number between 1 and 3,000.\n"))
+#         return "Number outside of operating parameters."
+#     else:
+#         return userInput
 
-# numbers = [numerals[x] for x in testNumber.lower()]
+numbers = [numerals[x] for x in testNumber.lower()]
 
-# print(numbers)
-
-if __name__ == "__main__":
-    # while running == True:
-    #     print(check_if_input_is_in_range())
-    numbers = [numerals[x] for x in testNumber.lower()]
-    total = 0
-    for value in numbers:
-        if value < numbers.index(value) + 1:
-            total -= value
+for index, value in enumerate(numbers):
+    if index + 1 <= len(numbers) - 1:
+        if value < numbers[index + 1]:
+            total += -value
         else:
             total += value
-    print(total)
+
+print(total)
+
+# for index, value in enumerate(numbers):
+#     if index + 1 < 10:
+#         if value < numbers[index] + 1:
+#             total += -value
+#         else:
+#             total += value
+# print(total)
+
+# print(numbers[numbers.index(50) + 1])
+
+# for index, value in enumerate(numbers):
+#     print(index, value)
+
+# if __name__ == "__main__":
+#     # while running == True:
+#     #     print(check_if_input_is_in_range())
+#     numbers = [numerals[x] for x in testNumber.lower()]
+#     for index, value in enumerate(numbers):
+#         if index + 1 <= len(numbers):
+#             if value < numbers[index + 1]:
+#                 total -= value
+#             else:
+#                 total += value
+#     print(total)
