7e57d02ca7da13c2efa8be09d1938b4b7ec2d4ee
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-09-06 08:25:30 -0500

    I think I can read the numerals now
    At the very least it works with MMM (which is 3,000 in Arabic numerals)

--- roman_numerals.py
+++ roman_numerals.py
@@ -7,3 +7,30 @@ numerals = {
     "d" : 500,
     "m" : 1000
 }
+
+running = True
+testNumber = "MMM"
+
+def check_if_input_is_in_range():
+    userInput = int(input("What roman number do you want to convert? (1 - 3,000)\n"))
+    if userInput < 1 or userInput > 3000:
+        userInput = int(input("Please enter a number between 1 and 3,000.\n"))
+        return "Number outside of operating parameters."
+    else:
+        return userInput
+
+# numbers = [numerals[x] for x in testNumber.lower()]
+
+# print(numbers)
+
+if __name__ == "__main__":
+    # while running == True:
+    #     print(check_if_input_is_in_range())
+    numbers = [numerals[x] for x in testNumber.lower()]
+    total = 0
+    for value in numbers:
+        if value < numbers.index(value) + 1:
+            total -= value
+        else:
+            total += value
+    print(total)
