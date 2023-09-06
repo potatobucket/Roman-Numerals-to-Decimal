8a33ca5bc0f02d1b81e3b8b4a92f7dc6ada18062
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-09-06 15:11:48 -0500

    It works
    It works now but I couldn't find a more elegant solution to the "would just skip over the last numeral" problem. Them's the breaks, I guess.

--- roman_numerals.py
+++ roman_numerals.py
@@ -8,52 +8,21 @@ numerals = {
     "m" : 1000
 }
 
-running = True
-testNumber = "MMM"
-# testNumber = "MCMLXXXVI"
+# testNumber = "MMM"
+testNumber = "MCMLXXXVI"
 # testNumber = "IV"
-total = 0
 
-# def check_if_input_is_in_range():
-#     userInput = int(input("What roman number do you want to convert? (1 - 3,000)\n"))
-#     if userInput < 1 or userInput > 3000:
-#         userInput = int(input("Please enter a number between 1 and 3,000.\n"))
-#         return "Number outside of operating parameters."
-#     else:
-#         return userInput
+def roman_numeral_conversion(numeral):
+    numbers = [numerals[x] for x in numeral.lower()]
+    total = 0
+    for index, value in enumerate(numbers):
+        if index + 1 <= len(numbers) - 1:
+            if value < numbers[index + 1]:
+                total += -value
+            else:
+                total += value
+    total += numbers[-1]
+    return total
 
-numbers = [numerals[x] for x in testNumber.lower()]
-
-for index, value in enumerate(numbers):
-    if index + 1 <= len(numbers) - 1:
-        if value < numbers[index + 1]:
-            total += -value
-        else:
-            total += value
-
-print(total)
-
-# for index, value in enumerate(numbers):
-#     if index + 1 < 10:
-#         if value < numbers[index] + 1:
-#             total += -value
-#         else:
-#             total += value
-# print(total)
-
-# print(numbers[numbers.index(50) + 1])
-
-# for index, value in enumerate(numbers):
-#     print(index, value)
-
-# if __name__ == "__main__":
-#     # while running == True:
-#     #     print(check_if_input_is_in_range())
-#     numbers = [numerals[x] for x in testNumber.lower()]
-#     for index, value in enumerate(numbers):
-#         if index + 1 <= len(numbers):
-#             if value < numbers[index + 1]:
-#                 total -= value
-#             else:
-#                 total += value
-#     print(total)
+if __name__ == "__main__":
+    print(roman_numeral_conversion(testNumber))
